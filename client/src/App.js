f0104069fdffd6e13408bf665ac384e5b2d90b55
Lukas <lukyn.odehnal333@gmail.com>
2021-07-03 09:32:57 +0200

    fetching works

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React, { useState } from 'react'
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
-  );
+function PersonList() {
+    const [joke, setJoke] = useState("");
+    const [type, setType] = useState("")
+    async function getResponse(){
+        const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+        console.log(response.data);
+        setJoke(`${response.data.setup}  ... ${response.data.punchline}`);
+        setType(response.data.type)
+    }
+    return (
+        <div>
+            <h1>Person list</h1>
+            <button onClick={getResponse}>get a joke</button>
+            <h1>{joke}</h1>
+            <h2>Type: {type}</h2>
+        </div>
+    )
 }
 
-export default App;
+export default PersonList
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from "axios"
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `http://localhost:3000/`
+})
 
 ReactDOM.render(
   <React.StrictMode>
